bd1cbcb55f71c5355495772e8415735306c3cdb6
patrisor <p.sorys.at42@gmail.com>
2019-04-30 03:36:37 -0700

    updated validate() and get_mino() functions

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: patrisor <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 13:20:29 by patrisor          #+#    #+#             */
-/*   Updated: 2019/04/17 22:36:22 by patrisor         ###   ########.fr       */
+/*   Updated: 2019/04/30 02:49:57 by patrisor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 # define LIBFT_H
 # define BUFF_SIZE 42
 
-# include "fcntl.h"
-# include "unistd.h"
-# include "stdlib.h"
-# include "libft.h"
-# include "sys/types.h"
-# include "sys/uio.h"
+# include <fcntl.h>
+# include <inttypes.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 
 int				get_next_line(const int fd, char **line);
 int				ft_new_line(char **s, char **line, int fd, int bytes);
@@ -96,19 +96,6 @@ void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstadd(t_list **alst, t_list *n);
 void			ft_lstiter(t_list *lst, void (*f)(void *));
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
-
-typedef unsigned char		t_bool;
-typedef unsigned char		t_char;
-
-typedef unsigned char		t_uint8;
-typedef unsigned short		t_uint16;
-typedef unsigned long		t_uint32;
-typedef unsigned long long	t_uint64;
-
-typedef signed char			t_int8;
-typedef signed short		t_int16;
-typedef signed long			t_int32;
-typedef signed long long	t_int64;
 
 int				ft_pow(int x, int y);
 double			ft_sqrt(double x);
--- main.c
+++ main.c
@@ -6,21 +6,74 @@
 /*   By: patrisor <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/28 20:12:43 by patrisor          #+#    #+#             */
-/*   Updated: 2019/04/30 00:01:03 by patrisor         ###   ########.fr       */
+/*   Updated: 2019/04/30 03:31:13 by patrisor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 #include <fcntl.h>
 
+// TODO: DELETE
+#include <stdio.h>
 
+/*
+ * Returns the 16-bit tetromino if it is valid, else returns 0 if invalid.
+ */ 
+uint16_t	validate(uint16_t mino)
+{
+	// if the result of the bitwise AND operation is zero, then it will shift the bits to the left
+	while(!(mino & 0xF000)) // 0xF000 = 1111 0000 0000 0000
+		mino = mino << 4;
+	// the code runs under this condition if the result of the bitwise AND operation is Zero
+	while(!(mino & 0x8888)) // 0x8888 = 1000 1000 1000 1000
+		mino = mino << 1;
+	// if a tetromino like 1110 0000 0000 0000 shows up, the the code will run under the ondition.
+	if((mino & 0xE000) == 0xE000) // 0xE000 = 1110 0000 0000 0000
+		return((mino & 0x1E00) ? mino : 0); // 0x1E00 = 0001 1110 0000 0000
+	
+	return mino;
+}
+
+/*
+ * Returns single tetromino converted from String into unsigned 16 bit int
+ */
+uint16_t	get_mino(char *buf)
+{
+	uint16_t    mino;
+	int			i;
+	int			count;
+
+	mino = 0;
+	i = 0;
+	count = 0;
+	// Iterate through the string buffer to check for '\n', '#', '.'
+	while(buf[i] != '\0')
+	{
+		// TODO: DELETE
+		printf("%" PRIu16 "\n", mino);
+	
+		// if you stumble upon a tetrimino piece piece, '#', the counter goes up.
+		if(buf[i] == '#' && ++count)
+			// MOST IMPORTANT: where you manipulate a 16 bit unsigned int, based on how the buffer looks like
+			mino = mino | (1 << (15 - (i - (i / 5))));
+		// the only available option is '\0'. You have reached end of the file
+		else if (buf[i] != '.' && buf[i] != '\n')
+			return (0);
+		// If you are on a newline characterand the next position is not divisible by 5. meaning you are not at end of tetromino.
+		else if (buf[i] == '\n' && ((i + 1) % 5 != 0))
+			return (0);
+		++i; 
+	}
+	// if the count != 4 or i != 20 (the piece is invalid), return 0. Else pass it into the validate function. 
+	return((count != 4 || i != 20) ? 0 : validate(mino));
+}
 
 t_list		*create_list(int fd, char *buf, int *prev_read)
 {
 	t_list		*head;
 	t_list		*cur;
 	int			count;
-	t_uint16	bits;
+	uint16_t	bits;
 	int			bytes;
 
 	head = ft_lstnew(0, 0);
@@ -38,6 +91,9 @@ t_list		*create_list(int fd, char *buf, int *prev_read)
 		
 		// DELETE
 		ft_putstr(buf);
+
+		++count;
+
 	}
 	return head;
 }
